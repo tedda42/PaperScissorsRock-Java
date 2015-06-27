aeccc434530dd4afaa0fd4996e1cfc8ce884bb30
tedda42 <tedda42@gmail.com>
2015-06-27 12:24:33 +1000

    initial commit

---
+++ src/com/github/tedda42/psr/Main.java
@@ -0,0 +1,8 @@
+package com.github.tedda42.psr;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Do you choose rock, paper or scissors?");
+    }
+}
