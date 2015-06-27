9b7c48039ce5e3014ad7867a0f1d316948df3842
tedda42 <tedda42@gmail.com>
2015-06-27 12:33:19 +1000

    Read in user choice

--- src/com/github/tedda42/psr/Main.java
+++ src/com/github/tedda42/psr/Main.java
@@ -1,8 +1,15 @@
 package com.github.tedda42.psr;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Do you choose rock, paper or scissors?");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String userChoice = br.readLine();
+        System.out.println("You entered = " + userChoice);
     }
 }
